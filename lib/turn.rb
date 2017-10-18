3540a1df6cbcd7ba9edd0747ea33c2fa17b89dd0
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-18 15:47:23 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,34 +3,36 @@ def display_board(board)
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[6]} | #{board[6]} "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-def input_to_index(input)
-  string.to_i - 1
+ 
+ 
+def input_to_index(user_input)
+  user_input.to_i - 1
 end
-
-def position_taken?(board, index)
-  board[index] == "X" || board[index] == "O"
-end
-
-
-def move(board, index, token = "X")
-  board[index] = token
-end
-
+ 
 def valid_move?(board, index)
-  index.between(0,8) && !position_taken?(board, index)
+      if board[index] == "O" || board[index] == "X" || !index.between?(0, 8)
+        false
+      elsif index.between?(0, 8)
+      true
+    end
+end
+
+def move(board, index, value = "X")
+  board[index] = value
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
+  puts "Please enter 1-9:" 
+  user_input = gets.strip.to_i 
+  index = input_to_index(user_input)
+
   if valid_move?(board, index)
-    move(board, index, token = "X")
+    move(board, index)
     display_board(board)
   else
-    input = gets.strip
+    puts "Invalid move"
+    turn(board)
   end
 end
