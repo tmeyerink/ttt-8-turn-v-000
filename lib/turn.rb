eb5e4f4713b4df0d1293e9c5067ffa1b4b513425
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-18 15:47:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,15 +5,15 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
- 
- 
+
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
- 
+
 def valid_move?(board, index)
       if board[index] == "O" || board[index] == "X" || !index.between?(0, 8)
-        false
+      false
       elsif index.between?(0, 8)
       true
     end
@@ -25,7 +25,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:" 
-  user_input = gets.strip.to_i 
+  user_input = gets.strip.to_i
   index = input_to_index(user_input)
 
   if valid_move?(board, index)
