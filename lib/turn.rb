008cb90f6d9b104e83b9fa0548d8870db1860df9
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-18 15:50:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -25,7 +25,7 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
-  user_input = gets.strip.to_i
+  user_input = gets.strip
   index = input_to_index(user_input)
 
   if valid_move?(board, index)
